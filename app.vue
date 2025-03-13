2127a6d2d00d57a806df94ea8bdcc95a65124bb8
Greemv <hvitl@mail.ru>
2025-03-13 23:17:56 +0500

    fx

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+    <!--  <NuxtLayout>-->
+    <!--    <NuxtPage />-->
+    <!--  </NuxtLayout>-->
+    <Html>
+    <Head>
+        <link as="style" href="/assets/css/styles.css" onload="this.onload=null;this.rel='stylesheet'" rel="preload">
+        <noscript>
+            <link href="/assets/css/styles.css" rel="stylesheet">
+        </noscript>
+    </Head>
+    <NuxtLayout>
+        <NuxtPage/>
+    </NuxtLayout>
+    </Html>
 </template>
+<script lang="ts" setup>
+</script>
