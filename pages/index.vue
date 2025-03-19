f2929b32e3f95117a1ce11a7023e076e830ff3af
Greemv <hvitl@mail.ru>
2025-03-19 11:34:16 +0500

    fx

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="flex items-center justify-center flex-col w-full h-screen ">
       <NuxtLink to="/login">login</NuxtLink>
-      <NuxtLink to="/platform/">platform</NuxtLink>
+<!--      <NuxtLink to="/platform/">platform</NuxtLink>-->
       <NuxtLink to="/assistants/create">assistant</NuxtLink>
       <NuxtLink to="/assistants">assistants</NuxtLink>
       <NuxtLink to="/knowledge">knowledge</NuxtLink>
